7b3ef8fb74efd0b5f141a8c6961ae02671c6cf96
404tk <54827262+404tk@users.noreply.github.com>
2025-12-04 20:57:28 +0800

    移除多余代码

--- sources/result.go
+++ sources/result.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Result struct {
-	IP          string   `json:"ip" excel:"name:IP;"`
-	Port        string   `json:"port" excel:"name:端口;"`
-	Protocol    string   `json:"protocol" excel:"name:服务;"`
+	IP          string   `json:"ip"`
+	Port        string   `json:"port"`
+	Protocol    string   `json:"protocol"`
 	Host        []string `json:"host"`
-	Url         string   `json:"url" excel:"name:URL;"`
-	Title       string   `json:"title" excel:"name:网站标题;"`
-	Fingerprint string   `json:"fingerprint" excel:"name:指纹;"`
-	Source      string   `json:"source" excel:"name:来源;"`
-	Prompt      string   `json:"prompt" excel:"name:查询语句;"`
-	LastUpdate  string   `json:"lastupdate" excel:"name:更新时间;"`
+	Url         string   `json:"url"`
+	Title       string   `json:"title"`
+	Fingerprint string   `json:"fingerprint"`
+	Source      string   `json:"source"`
+	Prompt      string   `json:"prompt"`
+	LastUpdate  string   `json:"lastupdate"`
 	Timestamp   int64    `json:"timestamp"`
 	Error       error    `json:"-"`
 }
@@ -36,39 +36,4 @@ func (r *Result) PrettyPrint() string {
 func (r *Result) JSON() string {
 	data, _ := json.Marshal(r)
 	return string(data)
-}
-
-type ResultSet []Result
-
-func NewResultSet(a ...Result) *ResultSet {
-	s := &ResultSet{}
-	s.Add(a...)
-	return s
-}
-
-func (this *ResultSet) Add(n ...Result) {
-	for _, r := range n {
-		*this = append(*this, r)
-	}
-}
-
-func (this *ResultSet) AsArray() []Result {
-	a := []Result{}
-	for _, n := range *this {
-		a = append(a, n)
-	}
-	return a
-}
-
-type IpDomain struct {
-	IP     string `excel:"name:IP;"`
-	Domain string `excel:"name:域名;"`
-}
-
-func IpDomainArray(ip string, hosts []string) []IpDomain {
-	res := []IpDomain{}
-	for _, host := range hosts {
-		res = append(res, IpDomain{IP: ip, Domain: host})
-	}
-	return res
 }
